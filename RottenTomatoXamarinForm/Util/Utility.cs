70508b9ee55824250dbb711fcef9108a307d1828
eternaltung <eternaltung@gmail.com>
2016-02-21 00:43:19 +0800

    commit project

---
+++ RottenTomatoXamarinForm/Util/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RottenTomatoXamarinForm.Util
+{
+    public class Utility
+    {
+        public static async Task<List<Movie>> GetMovieData(string url)
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync(url);
+            return JsonConvert.DeserializeObject<MovieModel>(json).movies;
+        }
+    }
+
+    
+}
---
+++ RottenTomatoXamarinForm/View/MainView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RottenTomatoXamarinForm
+{
+	public partial class MainView : TabbedPage
+	{
+        const string apikey = "";
+		List<Movie> movies;
+
+		public MainView ()
+		{
+			InitializeComponent ();
+            
+            UpdateUI();
+		}
+
+        private async Task UpdateUI()
+        {
+            movies = await Util.Utility.GetMovieData($"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey={apikey}&limit=20");
+            MovieListView.ItemsSource = movies;
+            
+        }
+	}
+}
+
